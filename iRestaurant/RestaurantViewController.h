e4f4b0cb6a8f45f3ac60206b6edd36da4c3f1dca
Josh Timonen <joshtimonen@gmail.com>
2011-04-12 17:25:51 -0700

    Working on restaurant model.

---
+++ iRestaurant/RestaurantViewController.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantViewController.h
+//  iRestaurant
+//
+//  Created by Josh Timonen on 4/12/11.
+//  Copyright 2011 N/A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RestaurantViewController : UITableViewController {
+    
+}
+
+@end
