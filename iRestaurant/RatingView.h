bec12da6087a57d4a80523e7240c7e25902294fd
Josh Timonen <joshtimonen@gmail.com>
2011-04-15 13:25:16 -0700

    tweaks

---
+++ iRestaurant/RatingView.h
@@ -0,0 +1,16 @@
+//
+//  RatingView.h
+//  iRestaurant
+//
+//  Created by Josh Timonen on 4/15/11.
+//  Copyright 2011 N/A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RatingView : UIView {
+    
+}
+
+@end
---
+++ iRestaurant/RatingView.m
@@ -0,0 +1,39 @@
+//
+//  RatingView.m
+//  iRestaurant
+//
+//  Created by Josh Timonen on 4/15/11.
+//  Copyright 2011 N/A. All rights reserved.
+//
+
+#import "RatingView.h"
+
+
+@implementation RatingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
