7be13f1646fb7b997ace8d8df25480d76ca714aa
Josh Timonen <joshtimonen@gmail.com>
2011-05-06 18:36:14 -0700

    working on bookmark service

---
+++ iRestaurant/RestaurantBookmarkService.h
@@ -0,0 +1,16 @@
+//
+//  RestaurantBookmarkService.h
+//  iRestaurant
+//
+//  Created by Josh Timonen on 5/6/11.
+//  Copyright 2011 N/A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RestaurantBookmarkService : NSObject {
+    
+}
+
+@end
---
+++ iRestaurant/RestaurantBookmarkService.m
@@ -0,0 +1,14 @@
+//
+//  RestaurantBookmarkService.m
+//  iRestaurant
+//
+//  Created by Josh Timonen on 5/6/11.
+//  Copyright 2011 N/A. All rights reserved.
+//
+
+#import "RestaurantBookmarkService.h"
+
+
+@implementation RestaurantBookmarkService
+
+@end
