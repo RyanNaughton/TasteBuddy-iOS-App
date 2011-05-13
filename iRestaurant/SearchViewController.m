5a37c6426be53a2cc9fb4d535812c8e631c0d7dc
Andrew Chalkley <andrew@chalkley.org>
2011-05-14 00:13:25 +0100

    No results on search

--- iRestaurant/SearchViewController.m
+++ iRestaurant/SearchViewController.m
@@ -105,7 +105,7 @@
     CGPoint point = CGPointMake(1.2345, 1.2345);
     searchService = [[SearchService alloc]initWithLocation:point withDelegate:self];
 
-    //[self presentSearchModal];
+    [self switchSearchView:restaurantsTabButton]; //Show no results initially.
 }
 
 -(void) switchTabs:(UIButton *) onTab 
