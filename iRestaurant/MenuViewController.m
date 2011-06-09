177f2cbc796ec8011763d50bfcc09d50712aeac6
Andrew Chalkley <andrew@chalkley.org>
2011-06-10 00:30:46 +0100

    Included header

--- iRestaurant/MenuViewController.m
+++ iRestaurant/MenuViewController.m
@@ -14,6 +14,7 @@
 #import "MenuSubcategory.h"
 #import "SimpleMenuTableView.h"
 #import "SectionsMenuTableView.h"
+#import "ASIFormDataRequest.h"
 
 @implementation MenuViewController
 @synthesize restaurant, menuService, tableView, activityIndicator, menuItems;
